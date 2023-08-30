217bd3cbc5458f7d632648183f6f00319abdb6e4
AndrewKHD <khudzik28@gmail.com>
2023-08-30 14:32:36 +0300

    ui and refresh logic

--- RemoteData/ContentView.swift
+++ RemoteData/ContentView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    let api = ApiService()
+    @State var jokes: [Joke] = []
+    @State var isOnlyFavorities: Bool = false
+    @State var isLoading: Bool = true
+    @State var isError: Bool = false
+    @State var errorReason: ApiError? = nil
     var body: some View {
-        Text("Hello World!")
+        VStack {
+            HStack {
+                Spacer()
+                Button {
+                    isOnlyFavorities.toggle()
+                } label: {
+                    Text(isOnlyFavorities ? "All" : "Favorites")
+                }
+            }
+            if isLoading {
+                Spacer()
+                ProgressView()
+                Spacer()
+            } else {
+                List {
+                    ForEach(jokes, id: \.id) { joke in
+                        CustomRow(joke: joke)
+                    }
+                }
+                .listStyle(.grouped)
+                .refreshable {
+                    await refresh()
+                }
+            }
+        }
+        .task {
+            isLoading = true
+            await refresh()
+            isLoading = false
+        }
+    }
+    
+    private func refresh() async {
+        do {
+            jokes = try await api.fetchJokes()
+        } catch {
+            
+        }
     }
 }
 
