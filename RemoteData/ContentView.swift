100a0e5bde0f4a925f063f90dddc1820eb728af9
AndrewKHD <khudzik28@gmail.com>
2023-08-30 13:37:48 +0300

    Initial Commit

---
+++ RemoteData/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  RemoteData
+//
+//  Created by Андрей Худик on 8/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemoteData/RemoteDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteDataApp.swift
+//  RemoteData
+//
+//  Created by Андрей Худик on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct RemoteDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
